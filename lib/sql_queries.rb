4a2499296f7d4a39c91def36b1f8ce3ec66c2ed6
Loren Cipriano <loren25cipriano@gmail.com>
2020-03-13 19:34:38 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT bears.temperament FROM bears GROUP BY temperament COUNT temperament='goofy'"
+  "SELECT bears.temperament FROM bears GROUP BY temperament COUNT(temperament)"
 end
 
 def selects_bear_that_killed_Tim
